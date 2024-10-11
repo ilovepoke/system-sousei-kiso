515c47c5bbc1f3df2070220a928fca2ab6e79787
ilovepoke <t226157@st.yamagata-u.ac.jp>
2024-10-11 19:12:33 +0900

    Create github2.c

---
+++ github2.c
@@ -0,0 +1,14 @@
+/*Ayana Shindo*/
+/*2024 Oct.11*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi, \tthere.\n");
+    
+    int this_year;
+    this_year = 2024;
+    printf("This year is: %d\n",this_year);
+
+    return 0;
+}
